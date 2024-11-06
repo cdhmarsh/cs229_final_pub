78547949a09f70265231cc3f3a831f4c34b268d2
Jack Le <jackle61333@gmail.com>
2024-11-06 14:38:51 -0800

    add baselines

---
+++ get_data.sh
@@ -0,0 +1,13 @@
+# Create data directories
+mkdir -p data/cifar-10h
+mkdir -p data/cifar-10
+
+# Download CIFAR-10H data
+wget -P data/cifar-10h https://raw.githubusercontent.com/jcpeterson/cifar-10h/master/data/cifar10h-counts.npy
+wget -P data/cifar-10h https://raw.githubusercontent.com/jcpeterson/cifar-10h/master/data/cifar10h-probs.npy
+
+# Download CIFAR-10 dataset
+# This will be handled by torchvision when you first run the code
+# But we can pre-download it using curl
+wget -P data/cifar-10 https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+tar -xzf data/cifar-10/cifar-10-python.tar.gz -C data/cifar-10
